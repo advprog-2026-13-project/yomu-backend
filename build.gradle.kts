fbd0dbc0e798ec9d92b51ff9172ba06df8eff327
Rifqi Ilham <muhammad.rifqi411@ui.ac.id>
2026-02-20 05:03:53 +0700

    add jacoco & linter

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     java
     id("org.springframework.boot") version "3.5.11"
     id("io.spring.dependency-management") version "1.1.7"
+
+    id("jacoco")
+    id("com.diffplug.spotless") version "6.25.0"
 }
 
 group = "id.ac.ui.cs.advprog"
@@ -28,18 +31,43 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
+
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-database-postgresql")
-    compileOnly("org.projectlombok:lombok")
-    testRuntimeOnly("com.h2database:h2")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
     runtimeOnly("org.postgresql:postgresql")
+
+    compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("com.h2database:h2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+jacoco {
+    toolVersion = "0.8.11"
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+}
+
+spotless {
+    java {
+        googleJavaFormat()
+        target("src/**/*.java")
+    }
 }
--- src/main/java/id/ac/ui/cs/advprog/yomu/backend/YomuBackendApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/yomu/backend/YomuBackendApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class YomuBackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(YomuBackendApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(YomuBackendApplication.class, args);
+  }
 }
