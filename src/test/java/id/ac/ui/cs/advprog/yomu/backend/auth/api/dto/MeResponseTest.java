930ac48e0b5dbf34d4d72c623845772d34c32275
Rifqi Ilham <muhammad.rifqi411@ui.ac.id>
2026-03-06 15:40:22 +0700

    add dto test

---
+++ src/test/java/id/ac/ui/cs/advprog/yomu/backend/auth/api/dto/MeResponseTest.java
@@ -0,0 +1,25 @@
+package id.ac.ui.cs.advprog.yomu.backend.auth.api.dto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import id.ac.ui.cs.advprog.yomu.backend.auth.domain.Role;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+
+class MeResponseTest {
+
+    @Test
+    void shouldCreateMeResponseCorrectly() {
+        UUID id = UUID.randomUUID();
+        String username = "rifqi";
+        String email = "rifqi@mail.com";
+        Role role = Role.USER;
+
+        MeResponse response = new MeResponse(id, username, email, role);
+
+        assertEquals(id, response.getId());
+        assertEquals(username, response.getUsername());
+        assertEquals(email, response.getEmail());
+        assertEquals(role, response.getRole());
+    }
+}
