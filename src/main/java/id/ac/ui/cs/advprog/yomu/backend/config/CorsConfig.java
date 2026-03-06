d9df0167d0c76dd581e0bd111ccc213f4b54c98e
nadyaaysha <nadyaaysha07@gmail.com>
2026-03-06 12:46:34 +0700

    Config

--- src/main/java/id/ac/ui/cs/advprog/yomu/backend/config/CorsConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/yomu/backend/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000") // URL frontend Next.js kamu
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
