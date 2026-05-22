e687c126c60825fd6ff8e26a7a4fc269b9367cc2
nadyaaysha <nadyaaysha07@gmail.com>
2026-05-22 19:34:51 +0700

    Profiling: Ngambil soal beserta opsi jawabannya dalam 1 query JOIN

--- src/main/java/id/ac/ui/cs/advprog/yomu/backend/reading/infrastructure/QuestionRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/yomu/backend/reading/infrastructure/QuestionRepository.java
@@ -3,10 +3,13 @@ package id.ac.ui.cs.advprog.yomu.backend.reading.infrastructure;
 import id.ac.ui.cs.advprog.yomu.backend.reading.domain.Question;
 import java.util.List;
 import java.util.UUID;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, UUID> {
+  @EntityGraph(attributePaths = {"options"})
   List<Question> findByReading_ReadingId(UUID readingId);
 }
